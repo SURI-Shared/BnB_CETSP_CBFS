e42173721531dee85802a65b371d3cb88e0fd01e
Geordan Gutow <ggutow@andrew.cmu.edu>
2024-05-16 18:29:00 -0400

    pass expected number of args to Data constructor

--- BnB_CETSP/test/test_SolveSocpClarabel.cpp
+++ BnB_CETSP/test/test_SolveSocpClarabel.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
     //Data constructor demands command line arguments
     //[path to instance] [OPTIONS] [OVERLAP FACTOR] [TIME LIMIT] [BRANCHING RULE] [BRANCHING STRATEGY] [ROOT SELECTION] [S.B SIZE]
-    vector<char*> load_args={"3D","1.0","1000","SB","BeFS","1","1","1"};
+    vector<char*> load_args={"test_SolveSocpClarabel","/home/ggutow/eclipse-workspace/BnB_CETSP/Behdani/CETSP-50-1.txt","3D","1.0","1000","SB","BeFS","1","1","1"};
     Data *dataptr = new Data("/home/ggutow/eclipse-workspace/BnB_CETSP/Behdani/CETSP-50-1.txt", "3D", 1, 9, load_args.data());
 
     vector<int> in_order;
