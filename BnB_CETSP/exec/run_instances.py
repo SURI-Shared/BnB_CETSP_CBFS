9f42b89a96cedac67b1dafb4b240026d3e3fe15a
Geordan Gutow <ggutow@andrew.cmu.edu>
2024-05-18 12:04:03 -0400

    running instances in parallel

--- BnB_CETSP/exec/run_instances.py
+++ BnB_CETSP/exec/run_instances.py
@@ -3,7 +3,9 @@ import glob
 import git
 import time
 import subprocess
+import multiprocessing
 import sys
+from itertools import zip_longest,repeat
 
 def write_README(outfolder,**kwargs):
     git_repo_path=os.path.join(os.path.dirname(__file__),"..","..")
@@ -14,11 +16,14 @@ def write_README(outfolder,**kwargs):
         for key,val in kwargs.items():
             fh.write(f"{key}: {val}\n")
 
-def runner(executable, infile_name,log_file_name,args):
-    print(f"Start {infile_name}")
+def runner(infile,executable,outfolder,args):
+    infile_name,ext=os.path.splitext(os.path.basename(infile))
+    print(f"Start {infile}")
+    log_file_name=os.path.join(outfolder,infile_name+".log")
     with open(log_file_name,"wt") as fh:
-        subprocess.run([executable,infile_name]+args,stdout=fh,stderr=fh)
-    print(f"Done {infile_name}")
+        subprocess.run([executable,infile]+args,stdout=fh,stderr=fh)
+    print(f"Done {infile}")
+
 def test_planner(processes,executable,in_folder,out_prefix,args):
     timestr=time.strftime("%Y%m%d_%H%M%S")
     outfolder=os.path.join(out_prefix,executable+"_"+timestr)
@@ -26,23 +31,13 @@ def test_planner(processes,executable,in_folder,out_prefix,args):
     write_README(outfolder,in_folder=in_folder,executable=executable,cmdline_args=args)
 
     infiles=glob.glob(os.path.join(in_folder,"*.txt"))
-    outfile_handles=[]
-    procs=[]
-    for infile in infiles:
-        print(infile)
-        infile_name,ext=os.path.splitext(os.path.basename(infile))
-        log_file_name=os.path.join(outfolder,infile_name+".log")
-        fh=open(log_file_name,"wt")
-        outfile_handles.append(fh)
-        procs.append(subprocess.Popen([executable,infile]+args,stdout=fh,stderr=fh))
-    for proc in procs:
-        proc.wait()
-    for fh in outfile_handles:
-        fh.close()
+    with multiprocessing.Pool(processes) as pool:
+        pool.starmap(runner,zip(infiles,repeat(executable),repeat(outfolder),repeat(args)))
 
 if __name__=="__main__":
-    executable=sys.argv[1]
-    in_folder=sys.argv[2]
-    out_prefix=sys.argv[3]
-    args=sys.argv[4:]
-    test_planner(executable,in_folder,out_prefix,args)
+    nprocesses=int(sys.argv[1])
+    executable=sys.argv[2]
+    in_folder=sys.argv[3]
+    out_prefix=sys.argv[4]
+    args=sys.argv[5:]
+    test_planner(nprocesses,executable,in_folder,out_prefix,args)
