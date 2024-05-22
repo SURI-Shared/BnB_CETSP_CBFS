15c0fb4ddd536b6111da2a8e3c4735dcee491b77
Geordan Gutow <ggutow@andrew.cmu.edu>
2024-05-22 19:49:33 -0400

    class to manage the computation of initial guesses for Clarabel
    need to check for off-by-one errors related to the sequence including the depot or not, but the object file compiles.

--- BnB_CETSP/src/warm_start_socp.cpp
+++ BnB_CETSP/src/warm_start_socp.cpp
@@ -1,8 +1,29 @@
 #include "warm_start_socp.h"
+#include <algorithm>
 
+WarmStartHandler::WarmStartHandler(){
+    //global matrices for the tiny insertion problem
+    Eigen::SparseMatrix<double> _insertion_problem_P(5,5);
+    Eigen::SparseMatrix<double> _insertion_problem_A(12,5);
+    Eigen::Matrix<double,5,1> _insertion_problem_q{1,1,0,0,0};
+    Eigen::Array<double,9,1> _insertion_problem_initial_b;
+    const std::vector<clarabel::SupportedConeT<double>> _insertion_problem_cones={clarabel::SecondOrderConeT<double>(4),clarabel::SecondOrderConeT<double>(4),clarabel::SecondOrderConeT<double>(4)};
+    clarabel::DefaultSettings<double> _insertion_problem_settings=clarabel::DefaultSettings<double>::rerunnable_settings();
+
+    insertion_problem_solver_ptr=new clarabel::DefaultSolver<double>(_insertion_problem_P,Eigen::Ref<Eigen::VectorXd>(_insertion_problem_q),
+                                                              _insertion_problem_A,Eigen::Ref<Eigen::VectorXd>(_insertion_problem_initial_b),
+                                                              _insertion_problem_cones,_insertion_problem_settings);
+}
+
+WarmStartHandler::~WarmStartHandler(){
+    delete insertion_problem_solver_ptr;
+}
 void WarmStartHandler::construct_initial_guess(const std::vector<int>& current_sequence, 
                              const std::vector<int>& parent_sequence, const Eigen::ArrayX3d & parent_turning_points, Eigen::Map<Eigen::VectorX<double>> parent_dual,
                              Data* instanceData,std::vector<double>& out_primals,std::vector<double>& out_slacks, std::vector<double>& out_duals){
+    //the actual decision variables are [fi...xi], which are the travel distances and the turning point coordinates respectively
+    //we use wi as shorthand for the displacement vector between x(i-1) and xi, wi=xi-x(i-1)
+    //and vi as shorthand for the displacement vector from xi to ci (the center of neighborhood i), vi=ci-xi
     size_t ndim=3;
     size_t per_turn=ndim+1;
     size_t m=current_sequence.size()-1;
@@ -22,8 +43,10 @@ void WarmStartHandler::construct_initial_guess(const std::vector<int>& current_s
     //for the new turning point, also computes the dual variable related to the constraint that the turning point stay in the neighborhood
     bool not_yet_inserted=true;
     bool not_in_parent=false;
+    std::vector<int> index_in_parent(current_sequence.size());
     int cnid;
     int pnid;
+    int pidx;
     Eigen::Vector3d depot={instanceData->getCoordx(current_sequence[0]),instanceData->getCoordy(current_sequence[0]),instanceData->getCoordz(current_sequence[0])};
     Eigen::Vector3d last_tp=depot;
     Eigen::Vector3d next_tp;
@@ -31,6 +54,7 @@ void WarmStartHandler::construct_initial_guess(const std::vector<int>& current_s
         if (not_yet_inserted){
             cnid=current_sequence.at(i);
             pnid=parent_sequence.at(i);
+            pidx=i;
             not_in_parent=pnid!=cnid;
             //once the neighborhood in current_sequence differs from that in parent, we have found the spot where a new neighborhood was inserted
             not_yet_inserted=not_in_parent;
@@ -38,8 +62,10 @@ void WarmStartHandler::construct_initial_guess(const std::vector<int>& current_s
             cnid=current_sequence.at(i);
             pnid=parent_sequence.at(i-1);
             not_in_parent=pnid!=cnid;
+            pidx=i-1;
         }
         if(not_in_parent){
+            index_in_parent.at(i)=-1;
             //guess the turning point to be at the optimal location if all other turning points are held fixed
             if(i+1>=current_sequence.size()){
                 next_tp=depot;
@@ -51,21 +77,84 @@ void WarmStartHandler::construct_initial_guess(const std::vector<int>& current_s
             solve_insertion_problem(last_tp,next_tp,center,radius,out_primals.data()+xstart+ndim*i,out_duals.data()+i*per_turn);
         }
         else{
+            index_in_parent.at(i)=pidx;
             //turning points in previously considered neighborhoods are guessed to not change
             last_tp=parent_turning_points.row(i);
-            out_primals.at(xstart+ndim*i)=last_tp[0];
-            out_primals.at(xstart+ndim*i+1)=last_tp[0];
-            out_primals.at(xstart+ndim*i+2)=last_tp[0];
+            out_primals[xstart+ndim*i]=last_tp[0];
+            out_primals[xstart+ndim*i+1]=last_tp[1];
+            out_primals[xstart+ndim*i+2]=last_tp[2];
         }
     }
 
     //populate f guesses
     last_tp=depot;
+    std::vector<Eigen::Vector3d> displacement_guesses;displacement_guesses.reserve(nf);
     for(size_t i=0;i<nf;i++){
-        next_tp=out_primals
-        Eigen::Vector3d displacement_guess=
+        next_tp=Eigen::Map<Eigen::Vector3d>(out_primals.data()+xstart+i*ndim);
+        displacement_guesses.push_back(next_tp-last_tp);
+        out_primals[i]=displacement_guesses[i].norm();
+        last_tp=next_tp;
     }
 
+    //slack guess
+    //set the bounding magnitude of vi slack as s=[radii[i], ci-xguessi^T]^T
+    for(size_t i=0;i<m;i++){
+        out_slacks[i*per_turn]=instanceData->getRadius(current_sequence[i]);
+
+        out_slacks[i*per_turn+1]=instanceData->getCoordx(current_sequence[i])-out_primals[xstart+ndim*i];
+        out_slacks[i*per_turn+2]=instanceData->getCoordy(current_sequence[i])-out_primals[xstart+ndim*i+1];
+        out_slacks[i*per_turn+3]=instanceData->getCoordz(current_sequence[i])-out_primals[xstart+ndim*i+2];
+    }
+
+    //set the bounding magnitude of wi slack as [fi wguessi^T]
+    size_t wmagstart=nx+m;
+    for (size_t i=0;i<nf;i++){
+        out_slacks[wmagstart+i*per_turn]=out_primals[i];
+        out_slacks[wmagstart+i*per_turn+1]=displacement_guesses[i][0];
+        out_slacks[wmagstart+i*per_turn+2]=displacement_guesses[i][1];
+        out_slacks[wmagstart+i*per_turn+3]=displacement_guesses[i][2];
+    }
+
+    //dual guess
+    //dual variables are paired with slack variables
+    //for those constraints that persist from the parent we can just reuse the parent's dual solution
+    //constraints are (in order that they appear in the constraint matrix A)
+    //m SOC constraints bounding magnitude of ci-xi (ndim+1 rows per SOC constraint, so nx+m total rows)
+    //nf SOC constraints relating magnitude of wi to fi (ndim+1 rows per SOC constraint, so nw+nf total rows)
+
+    //ci-xi
+    for(size_t i=0;i<current_sequence.size();i++){
+        if (index_in_parent.at(i)>=0){
+            //reuse dual values for SOC constraints bounding the magnitude of ci-xi
+            for(size_t j=0;j<per_turn;j++){
+                out_slacks[i*per_turn+j]=parent_dual[index_in_parent.at(i)*per_turn+j];
+            }
+        }else{
+            //SOC constraints enforcing ||ci-xi||<=ri
+            //at the solution the slacks will be [ri ci-xi] and the duals [lambda_i, -lambda_i*(ci-xi)/||ci-xi||]
+            //where lamgda_i is the lagrange multiplier of the constraint ||ci-xu||<=ri
+            //consider the optimization to pick the ith turning point holding the turn before and after it constant
+            //this optimization has cost function (f_i+f_{i+1})/2 and its only constraint is ||ci-xi||**2<=ri**2
+            //from the KKT conditions, specifically the 0 gradient of the Lagrangian, we obtain:
+            //w_i/f_i/2-w_{i+1}/f_{i+1}/2+2*lambda_i*(x-c)=0
+            Eigen::Map<Eigen::Vector3d> vguess(out_slacks.data()+wmagstart+i*per_turn+1);
+            double lambda_i=out_duals[i*per_turn];//populated by solve_insertion_problem
+            double d2center=vguess.norm();
+            out_duals[i*per_turn+1]=-lambda_i*vguess[0]/d2center;
+            out_duals[i*per_turn+2]=-lambda_i*vguess[1]/d2center;
+            out_duals[i*per_turn+3]=-lambda_i*vguess[2]/d2center;
+        }
+    }
+
+    //||wi||<=fi
+    for(size_t i=0;i<nf;i++){
+        //At the solution the slacks will be [fi wi.T] and the duals are [1 -wi.T/fi] so we use the wi and fi guess to construct the dual guess
+        out_duals[wmagstart+i*per_turn]=1;
+        
+        out_duals[wmagstart+i*per_turn+1]=-displacement_guesses[i][0]/out_primals[i];
+        out_duals[wmagstart+i*per_turn+2]=-displacement_guesses[i][1]/out_primals[i];
+        out_duals[wmagstart+i*per_turn+3]=-displacement_guesses[i][2]/out_primals[i];
+    }
 }
 
 /*
@@ -94,10 +183,10 @@ void WarmStartHandler::solve_insertion_problem(const Eigen::Vector3d& point1, co
     new_b.segment<3>(5)=-point1;
     new_b[8]=0;
     new_b.segment<3>(9)=-point2;
-    insertion_problem_solver.update_b(new_b);
+    insertion_problem_solver_ptr->update_b(new_b);
 
-    insertion_problem_solver.solve();
-    clarabel::DefaultSolution<double> solution=insertion_problem_solver.solution();
+    insertion_problem_solver_ptr->solve();
+    clarabel::DefaultSolution<double> solution=insertion_problem_solver_ptr->solution();
 
     for (size_t i=0;i<3;i++){
         out_turning_point[i]=solution.x[2+i];
--- BnB_CETSP/src/warm_start_socp.h
+++ BnB_CETSP/src/warm_start_socp.h
@@ -2,8 +2,6 @@
 #define WARM_START_SOCP_H
 
 #include <vector>
-#include "Eigen/Eigen"
-
 #include <Clarabel>
 
 #include"Data.h"
@@ -11,13 +9,13 @@
 class WarmStartHandler{
     public:
         WarmStartHandler();
-
+        ~WarmStartHandler();
         void construct_initial_guess(const std::vector<int>& current_sequence, 
                              const std::vector<int>& parent_sequence, const Eigen::ArrayX3d & parent_turning_points, Eigen::Map<Eigen::VectorX<double>> parent_dual,
                              Data* instanceData,std::vector<double>& out_primals,std::vector<double>& out_slacks, std::vector<double>& out_duals);
         void solve_insertion_problem(const Eigen::Vector3d&, const Eigen::Vector3d&, const Eigen::Vector3d&,double,double*,double*);
     protected:
-        clarabel::DefaultSolver<double> insertion_problem_solver;
+        clarabel::DefaultSolver<double>* insertion_problem_solver_ptr;
 
 };
 
