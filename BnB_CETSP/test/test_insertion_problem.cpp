cc6492699bccffe3dfae5a1ead8eded4340a82ab
Geordan Gutow <ggutow@andrew.cmu.edu>
2024-05-24 16:20:39 -0400

    add test script for insertion SOCP used for warmstarting

---
+++ BnB_CETSP/test/test_insertion_problem.cpp
@@ -0,0 +1,17 @@
+#include "warm_start_socp.h"
+#include "clarabel_interface/SolveSocpClarabelWithRecycling.h"
+
+int main(int argc, char** argv) 
+{
+    WarmStartHandler warm_start_handler;
+    double dual_value;
+    double turning_point[3];
+    Eigen::Vector3d center={0,0,0};
+    Eigen::Vector3d p1={0,2,0};
+    Eigen::Vector3d p2={2,0,0};
+    double r=1;
+    warm_start_handler.solve_insertion_problem(p1,p2,center,r,turning_point,&dual_value);
+    cout<<"Turning Point: "<<turning_point[0]<<", "<<turning_point[1]<<", "<<turning_point[2]<<endl;
+    cout<<"Dual: "<<dual_value<<endl;
+    return 0;
+}
