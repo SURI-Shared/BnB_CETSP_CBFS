a15bb642a24cf59b02853d8883c73bdd49f2868a
Geordan Gutow <ggutow@andrew.cmu.edu>
2024-08-29 14:46:33 -0400

    for Clarabel, clear the setup timers when updating the b vector so we don't double count the problem setup time when reusing the solver objects

--- BnB_CETSP/src/clarabel_interface/SolveSocpClarabelWithRecycling.cpp
+++ BnB_CETSP/src/clarabel_interface/SolveSocpClarabelWithRecycling.cpp
@@ -40,6 +40,7 @@ void SolveSocpClarabelWithRecycling::createModel(vector<int>& sequence){
 void SolveSocpClarabelWithRecycling::solveSOCP(){
     SolveSocpClarabel::solveSOCP();
     accumulate_info();
+    cout<<solution.setup_time<<endl;
 }
 void SolveSocpClarabelWithRecycling::accumulate_info(){
     SolveSocpClarabel::accumulate_info(info_struct);
@@ -115,6 +116,7 @@ void SolveSocpClarabelWithRecycling::update_b(){
             }
         }
     }
+    solver_ptr->clear_setup_timers();
     solver_ptr->update_b(b);
 }
 void SolveSocpClarabelWithRecycling::clear_removable_constraints(){}
@@ -144,4 +146,5 @@ void SolveSocpClarabelWithRecycling::solve_warm(double* primal_guess, double* sl
     violation=solution.r_prim;
     m_num_solves+=1;
     accumulate_info();
+    cout<<solution.setup_time<<endl;
 }
